9c91588bc4c63002dece774566e00e9289c99400
kakl <kakl@astro.cz>
2019-02-12 20:25:23 +0100

    two detectors

--- sw/divisek/divisek.ino
+++ sw/divisek/divisek.ino
@@ -1,11 +1,8 @@
-//#define DEBUG
 String githash = "$Id$";
 /*
-  AIRDOS with RTC
+  Divisek
  
 Compiled with: Arduino 1.8.5
-
-3 month endurance with LS 33600 = 7.6 mA
 
 ISP
 ---
@@ -28,7 +25,7 @@ RESET  0   PB0
 
 LED
 ---
-LED_yellow  23  PC7         // LED for Dasa
+LED  23  PC7         // LED for Dasa
 
 
                      Mighty 1284p    
@@ -61,7 +58,7 @@ TX1/INT1 (D 11) PD3 17|        |24 PC2 (D 18) TCK
 #include <Wire.h>           // Tested with version 1.0.0.
 #include "RTClib.h"         // Tested with version 1.5.4.
 
-#define LED_yellow  23 // PC7
+#define LED       23   // PC7
 #define RESET     0    // PB0
 #define SDpower1  1    // PB1
 #define SDpower2  2    // PB2
@@ -70,38 +67,58 @@ TX1/INT1 (D 11) PD3 17|        |24 PC2 (D 18) TCK
 #define MOSI      5    // PB5
 #define MISO      6    // PB6
 #define SCK       7    // PB7
-#define INT       20   // PC4
-
-#define CHANNELS 512 // number of channels in buffer for histogram, including negative numbers
+#define INTA      24   // PA0
+#define INTB      25   // PA1
 
 uint16_t count = 0;
 uint32_t serialhash = 0;
 
-int8_t lightning[32];
+uint8_t lightning[32];
+String dataString = "";
+char buf[10];
 
 RTC_Millis rtc;
 
-// Read Analog Differential without gain (read datashet of ATMega1280 and ATMega2560 for refference)
-// Use analogReadDiff(NUM)
-//   NUM  | POS PIN             | NEG PIN           |   GAIN
-//  0 | A0      | A1      | 1x
-//  1 | A1      | A1      | 1x
-//  2 | A2      | A1      | 1x
-//  3 | A3      | A1      | 1x
-//  4 | A4      | A1      | 1x
-//  5 | A5      | A1      | 1x
-//  6 | A6      | A1      | 1x
-//  7 | A7      | A1      | 1x
-//  8 | A8      | A9      | 1x
-//  9 | A9      | A9      | 1x
-//  10  | A10     | A9      | 1x
-//  11  | A11     | A9      | 1x
-//  12  | A12     | A9      | 1x
-//  13  | A13     | A9      | 1x
-//  14  | A14     | A9      | 1x
-//  15  | A15     | A9      | 1x
-#define PIN 0
-uint8_t analog_reference = INTERNAL2V56; // DEFAULT, INTERNAL, INTERNAL1V1, INTERNAL2V56, or EXTERNAL
+void DataOut()
+{
+  DateTime now = rtc.now();
+
+  uint32_t stroke, stroke_energy;
+
+  dataString += "$STROKE,";
+
+  dataString += String(char('A' + lightning[30] - 1)); 
+  dataString += ",";
+
+  dataString += String(count); 
+  dataString += ",";
+
+  dataString += String(now.unixtime());  // Time of discharge
+  dataString += ",";
+
+  dataString += String(lightning[3]);  // Type of discharge
+  dataString += ",";
+
+  stroke_energy = lightning[4];
+  stroke = lightning[5];
+  stroke_energy += stroke << 8;
+  stroke = lightning[6] & 0b11111;
+  stroke_energy += stroke << 16;
+
+  dataString += String(stroke_energy);  // Energy of single stroke
+  dataString += ",";
+
+  dataString += String(lightning[7] & 0b111111);  // Distance from storm
+
+  for (int8_t n=0; n<9; n++)
+  {
+    dataString += ",";
+    sprintf(buf, "0x%02X", lightning[n]);
+    dataString += buf;
+  }
+
+  dataString += "\r\n";
+}
 
 void setup()
 {
@@ -114,27 +131,6 @@ void setup()
   }
 
   Serial.println("#Cvak...");
-  
-  ADMUX = (analog_reference << 6) | ((PIN | 0x10) & 0x1F);
-  
-  ADCSRB = 0;               // Switching ADC to Free Running mode
-  sbi(ADCSRA, ADATE);       // ADC autotrigger enable (mandatory for free running mode)
-  sbi(ADCSRA, ADSC);        // ADC start the first conversions
-  sbi(ADCSRA, 2);           // 0x100 = clock divided by 16, 62.5 kHz, 208 us for 13 cycles of one AD conversion, 24 us fo 1.5 cycle for sample-hold
-  cbi(ADCSRA, 1);        
-  cbi(ADCSRA, 0);        
-
-  pinMode(RESET, OUTPUT);   // reset for peak detetor
-
-  pinMode(INT, INPUT);      // interrupt from lightning detector
-     
-  //pinMode(SDpower1, OUTPUT);  // SDcard interface
-  //pinMode(SDpower2, OUTPUT);     
-  //pinMode(SDpower3, OUTPUT);     
-  //pinMode(SS, OUTPUT);     
-  //pinMode(MOSI, INPUT);     
-  //pinMode(MISO, INPUT);     
-  //pinMode(SCK, OUTPUT);  
 
   DDRB = 0b10011110;
   PORTB = 0b00000000;  // SDcard Power OFF
@@ -146,24 +142,21 @@ void setup()
   DDRD = 0b11111100;
   PORTD = 0b00000000;  // SDcard Power OFF
 
-  pinMode(LED_yellow, OUTPUT);
-  digitalWrite(LED_yellow, LOW);  
-  digitalWrite(RESET, LOW);  
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);  
   
-  //!!! Wire.setClock(100000);
-
   for(int i=0; i<5; i++)  
   {
     delay(50);
-    digitalWrite(LED_yellow, HIGH);  // Blink for Dasa 
+    digitalWrite(LED, HIGH);  // Blink for Dasa 
     delay(50);
-    digitalWrite(LED_yellow, LOW);  
+    digitalWrite(LED, LOW);  
   }
 
   Serial.println("#Hmmm...");
 
   // make a string for device identification output
-  String dataString = "$AIRDOS,F," + githash.substring(5,44) + ","; // FW version and Git hash
+  String dataString = "$DIVISEK,01," + githash.substring(5,44) + ","; // FW version and Git hash
   
   Wire.beginTransmission(0x58);                   // request SN from EEPROM
   Wire.write((int)0x08); // MSB
@@ -201,9 +194,9 @@ void setup()
       dataFile.println(dataString);  // write to SDcard (800 ms)     
       dataFile.close();
   
-      digitalWrite(LED_yellow, HIGH);  // Blink for Dasa
+      digitalWrite(LED, HIGH);  // Blink for Dasa
       Serial.println(dataString);  // print to terminal (additional 700 ms in DEBUG mode)
-      digitalWrite(LED_yellow, LOW);          
+      digitalWrite(LED, LOW);          
     }  
     // if the file isn't open, pop up an error:
     else 
@@ -220,101 +213,80 @@ void setup()
 
 void loop()
 {
-  while(1)
+  lightning[30] = 0;
+  // make a string for assembling the data to log:
+  dataString = "";
+
+  if (digitalRead(INTA))
   {
-    if (digitalRead(INT))
-    {
-      delay(2); // minimal delay after stroke interrupt
-  
-      Wire.requestFrom((uint8_t)3, (uint8_t)8);    // request 9 bytes from slave device #3
-  
-      for (int8_t reg=0; reg<8; reg++)
-      { 
-        lightning[reg] = Wire.read();    // receive a byte
-      }
-      lightning[0] = 0;
-      break;
+    delay(100); // minimal delay after stroke interrupt
+
+    Wire.requestFrom((uint8_t)3, (uint8_t)8);    // request 9 bytes from slave device #3
+
+    for (int8_t reg=0; reg<8; reg++)
+    { 
+      lightning[reg] = Wire.read();    // receive a byte
     }
+    lightning[30] = 1;
+    DataOut();
+  }
+
+  if (digitalRead(INTB))
+  {
+    delay(100); // minimal delay after stroke interrupt
+
+    Wire.requestFrom((uint8_t)2, (uint8_t)8);    // request 9 bytes from slave device #3
+
+    for (int8_t reg=0; reg<8; reg++)
+    { 
+      lightning[reg] = Wire.read();    // receive a byte
+    }
+    lightning[30] = 2;
+    DataOut();
   }
 
   // Data out
+  
+
+  if (lightning[30])  
   {
-    DateTime now = rtc.now();
+    digitalWrite(LED, HIGH);  // Blink for Dasa
+    Serial.print(dataString);  // print to terminal (additional 700 ms in DEBUG mode)
+    digitalWrite(LED, LOW);          
 
-    // make a string for assembling the data to log:
-    String dataString = "";
+    DDRB = 0b10111110;
+    PORTB = 0b00001111;  // SDcard Power ON
 
+    // make sure that the default chip select pin is set to output
+    // see if the card is present and can be initialized:
+    if (!SD.begin(SS)) 
     {
-      uint32_t stroke, stroke_energy;
-  
-      dataString += "$STROKE,";
-
-      dataString += String(count); 
-      dataString += ",";
-
-      dataString += String(now.unixtime() - (9 - lightning[0]));  // Time of discharge
-      dataString += ",";
-
-      dataString += String(lightning[3]);  // Type of discharge
-      dataString += ",";
-
-      stroke_energy = lightning[4];
-      stroke = lightning[5];
-      stroke_energy += stroke << 8;
-      stroke = lightning[6] & 0b11111;
-      stroke_energy += stroke << 16;
-  
-      dataString += String(stroke_energy);  // Energy of single stroke
-      dataString += ",";
-    
-      dataString += String(lightning[7] & 0b111111);  // Distance from storm
-      dataString += "\r\n";
+      Serial.println("#Card failed, or not present");
+      // don't do anything more:
+      return;
     }
 
-    count++;
-
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  
+    // if the file is available, write to it:
+    if (dataFile) 
     {
-      DDRB = 0b10111110;
-      PORTB = 0b00001111;  // SDcard Power ON
+      dataFile.print(dataString);  // write to SDcard (800 ms)     
+      dataFile.close();
+    }  
+    // if the file isn't open, pop up an error:
+    else 
+    {
+      Serial.println("#error opening datalog.txt");
+    }
 
-      // make sure that the default chip select pin is set to output
-      // see if the card is present and can be initialized:
-      if (!SD.begin(SS)) 
-      {
-        Serial.println("#Card failed, or not present");
-        // don't do anything more:
-        return;
-      }
+    DDRB = 0b10011110;
+    PORTB = 0b00000001;  // SDcard Power OFF
 
-      // open the file. note that only one file can be open at a time,
-      // so you have to close this one before opening another.
-      File dataFile = SD.open("datalog.txt", FILE_WRITE);
-    
-      // if the file is available, write to it:
-      if (dataFile) 
-      {
-        //digitalWrite(LED_yellow, HIGH);  // Blink for Dasa
-        dataFile.println(dataString);  // write to SDcard (800 ms)     
-        //digitalWrite(LED_yellow, LOW);          
-        dataFile.close();
-
-#ifdef DEBUG
-        dataString.remove(100); 
-#endif
-        digitalWrite(LED_yellow, HIGH);  // Blink for Dasa
-        Serial.println(dataString);  // print to terminal (additional 700 ms in DEBUG mode)
-        digitalWrite(LED_yellow, LOW);          
-      }  
-      // if the file isn't open, pop up an error:
-      else 
-      {
-        Serial.println("#error opening datalog.txt");
-      }
-
-      DDRB = 0b10011110;
-      PORTB = 0b00000001;  // SDcard Power OFF
-    }          
-  }    
+    count++;
+  }          
 }
 
 
